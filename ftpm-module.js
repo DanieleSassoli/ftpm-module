217266d28d738782a9fe49077cfca7343d0f58b6
DanieleSassoli <danielesassoli@gmail.com>
2014-08-07 16:07:47 +0200

    Vairous bugfixes on cssfont.js, osfont.js and webfont.js

--- ftpm-module.js
+++ ftpm-module.js
@@ -1,33 +1,28 @@
 var ftpm = require('./lib/ftpm');
-
+var fs = require('fs');
 var ftpmModuleCb = {
 	installOsFont: function(fontName, cb) {
         ftpm.runDriver('osfont', 'install', fontName, cb);
 	},
 	uninstallOsFont: function(fontName, cb) {
 		fontName = fontName.toTitleCase();
-		ftpm.callbackFunction = cb;
-		if (existsSync(ftpm.path.getFontPath(ftpm.platform) + fontName.removeSpaces() + '.ftpm.ttf')) {
+		if (fs.existsSync(ftpm.path.getFontPath(ftpm.platform) + fontName.removeSpaces() + '.ftpm.ttf')) {
 			ftpm.runDriver('osfont', 'uninstall', fontName,cb);
 		}
 	},
 	listIntalledFonts: function(cb) {
-		ftpm.callbackFunction = cb;
 		ftpm.runDriver('osfont', 'local', '',cb);
 	},
 	downloadWebFont: function(fontName, fontPath, cb) {
-		ftpm.callbackFunction = cb;
 		ftpm.outputPath = fontPath;
 		ftpm.runDriver('webfont', 'web', fontName,cb);
 	},
-	getCssFont: function(fontName, fontPath, cb) {
-		ftpm.callbackFunction = cb;
+	getCssFont: function(fontName, fontPath,showContent, cb) {
 		ftpm.outputPath = fontPath;
 		ftpm.showContent = (!fontPath) ? true : false;
 		ftpm.runDriver('cssfont', 'css', fontName,cb);
 	},
 	getDataURI: function(fontName, fontPath, cb) {
-		ftpm.callbackFunction = cb;
 		ftpm.outputPath = fontPath;
 		ftpm.showContent = (!fontPath) ? true : false;
 		ftpm.runDriver('cssfont', 'datauri', fontName,cb);
